a6627ccab0220ea8b6f2c094afb942c1bf3f07b8
Julius Lungys <juliuslungys@gmail.com>
2019-02-16 17:44:59 +0200

    refactor: getLenteleX to classes getLentele

--- laboratorinis_1/laboratorinis_1.js
+++ laboratorinis_1/laboratorinis_1.js
@@ -1,65 +1,65 @@
 window.onload = () => {
-    const lentele1 = getLentele1();
+    const lentele1 = new Lentele1;
     const lab1Uzd1Document = document.getElementById('lab1-uzd1');
     Plotly.react(lab1Uzd1Document, [lentele1.data], lentele1.layout);
 
     const lab1Uzd2Document = document.getElementById('lab1-uzd2');
-    Plotly.react(lab1Uzd2Document, [getLentele2()]);
+    Plotly.react(lab1Uzd2Document, [new Lentele2]);
 
     //TODO: kolkas tik piesia taskus
-    const lentele3 = getLentele3();
+    const lentele3 = new Lentele3;
     const lab1Uzd3Document = document.getElementById('lab1-uzd3');
-    Plotly.react(lab1Uzd3Document, [lentele3.data]);
+    Plotly.react(lab1Uzd3Document, [lentele3.data], lentele3.layout);
 };
-
-function getLentele1() {
-    const data = {
-        x: [0, 10, -3, 4],
-        y: [3, 2, 1, 0],
-        mode: 'markers+text',
-        marker: {
-            color: 'rgba(156, 165, 196, 0.95)',
-            line: {
-                color: 'rgba(156, 165, 196, 1.0)',
-                width: 1,
+class Lentele1 {
+    constructor() {
+        this.data = {
+            x: [0, 10, -3, 4],
+            y: [3, 2, 1, 0],
+            mode: 'markers+text',
+            marker: {
+                color: 'rgba(156, 165, 196, 0.95)',
+                line: {
+                    color: 'rgba(156, 165, 196, 1.0)',
+                    width: 1,
+                },
+                symbol: 'circle',
+                size: 4
             },
-            symbol: 'circle',
-            size: 4
-        },
-        text: ['i1', 'i2', 'i3', 'i4'],
-        textposition: 'bottom',
-    };
-    const xMinRadius = Math.min.apply(Math, data.x) - 1;
-    const xMaxRadius = Math.max.apply(Math, data.x) + 1;
-    const yMinRadius = Math.min.apply(Math, data.y) - 1;
-    const yMaxRadius = Math.max.apply(Math, data.y) + 1;
-    return {
-        data,
-        layout: {
+            text: ['i1', 'i2', 'i3', 'i4'],
+            textposition: 'bottom',
+        };
+    }
+    get layout() {
+        const xMinRadius = Math.min.apply(Math, this.data.x) - 1;
+        const xMaxRadius = Math.max.apply(Math, this.data.x) + 1;
+        const yMinRadius = Math.min.apply(Math, this.data.y) - 1;
+        const yMaxRadius = Math.max.apply(Math, this.data.y) + 1;
+        return {
             xaxis: {
                 range: [xMinRadius, xMaxRadius]
             },
             yaxis: {
                 range: [yMinRadius, yMaxRadius]
             }
-        }
-    };
+        };
+    }
 }
 
-function getLentele2() {
-    const valuesLentele2 = [
-        [],
-        [],
-        []
-    ];
-    duomenysRackausko.forEach((value, index) => {
-        valuesLentele2[0].push('i' + index);
-        valuesLentele2[1].push(value.Pajamos);
-        valuesLentele2[2].push(value.Islaidos);
-    });
-    return {
-        type: 'table',
-        header: {
+class Lentele2 {
+    constructor() {
+        const values = [
+            [],
+            [],
+            []
+        ];
+        duomenysRackausko.forEach((value, index) => {
+            values[0].push('i' + index);
+            values[1].push(value.Pajamos);
+            values[2].push(value.Islaidos);
+        });
+        this.type = 'table';
+        this.header = {
             values: [
                 ['Duomenys Is Rackausko'],
                 ['Pajamos'],
@@ -78,9 +78,9 @@ function getLentele2() {
                 size: 12,
                 color: 'white'
             }
-        },
-        cells: {
-            values: valuesLentele2,
+        };
+        this.cells = {
+            values: values,
             align: 'center',
             line: {
                 color: 'black',
@@ -91,47 +91,48 @@ function getLentele2() {
                 size: 11,
                 color: ['black']
             }
-        }
-    };
+        };
+    }
 }
 
-function getLentele3() {
-    const data = {
-        x: [],
-        y: [],
-        mode: 'markers+text',
-        marker: {
-            color: 'rgba(156, 165, 196, 0.95)',
-            line: {
-                color: 'rgba(156, 165, 196, 1.0)',
-                width: 1,
+class Lentele3 {
+    constructor() {
+        this.data = {
+            x: [],
+            y: [],
+            mode: 'markers+text',
+            marker: {
+                color: 'rgba(156, 165, 196, 0.95)',
+                line: {
+                    color: 'rgba(156, 165, 196, 1.0)',
+                    width: 1,
+                },
+                symbol: 'circle',
+                size: 4
             },
-            symbol: 'circle',
-            size: 4
-        },
-        text: [],
-        textposition: 'bottom',
-    };
+            text: [],
+            textposition: 'bottom',
+        };
 
-    data.x = duomenysRackausko.map((object) => object['Pajamos']);
-    data.y = duomenysRackausko.map((object) => object['Islaidos']);
-    data.x.forEach((value, index) => {
-        data.text.push('i' + index);
-    });
-
-    const xMinRadius = Math.min.apply(Math, data.x) - 1;
-    const xMaxRadius = Math.max.apply(Math, data.x) + 1;
-    const yMinRadius = Math.min.apply(Math, data.y) - 1;
-    const yMaxRadius = Math.max.apply(Math, data.y) + 1;
-    return {
-        data,
-        layout: {
+        this.data.x = duomenysRackausko.map((object) => object['Pajamos']);
+        this.data.y = duomenysRackausko.map((object) => object['Islaidos']);
+        this.data.x.forEach((value, index) => {
+            this.data.text.push('i' + index);
+        });
+    }
+    get layout() {
+        const xMinRadius = Math.min.apply(Math, this.data.x) - 1;
+        const xMaxRadius = Math.max.apply(Math, this.data.x) + 1;
+        const yMinRadius = Math.min.apply(Math, this.data.y) - 1;
+        const yMaxRadius = Math.max.apply(Math, this.data.y) + 1;
+        return {
             xaxis: {
                 range: [xMinRadius, xMaxRadius]
             },
             yaxis: {
                 range: [yMinRadius, yMaxRadius]
             }
-        }
-    };
+
+        };
+    }
 }
