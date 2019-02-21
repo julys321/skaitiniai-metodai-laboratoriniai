341352b0821fa6ff34415ae266ee0f7189bc8a8a
Julius Lungys <juliuslungys@gmail.com>
2019-02-21 16:50:25 +0200

    feat(lab2uzd1): add data

---
+++ laboratorinis_1/laboratorinis_2_data.js
@@ -0,0 +1,35 @@
+/* exported laboratorinis2Uzd1Data */
+var laboratorinis2Uzd1Data = [
+    {
+        Diena: 1,
+        Euribor: 0.230
+    },
+    {
+        Diena: 3,
+        Euribor: 0.228
+    },
+    {
+        Diena: 4,
+        Euribor: 0.226
+    },
+    {
+        Diena: 5,
+        Euribor: 0.222
+    },
+    {
+        Diena: 7,
+        Euribor: 0.223
+    },
+    {
+        Diena: 8,
+        Euribor: 0.218
+    },
+    {
+        Diena: 9,
+        Euribor: 0.206
+    },
+    {
+        Diena: 10,
+        Euribor: 0.202
+    }
+];
