9adf0da79f34ccbfaf1db2420a5b47e8808d91cd
Julius Lungys <juliuslungys@gmail.com>
2019-03-02 14:18:36 +0200

    add lab2uzd1 draw of newton table

--- laboratorinis_2/laboratorinis_2.js
+++ laboratorinis_2/laboratorinis_2.js
@@ -1,6 +1,48 @@
+var data = [];
 window.onload = () => {
-    drawUzd1();
+    data = laboratorinis2Uzd1Data.map(e => {
+        return {
+            x: e.Euribor,
+            y: e.Diena
+        };
+    });
+    // data = [{
+    //     x: -3,
+    //     y: 469
+    // }, {
+    //     x: -2,
+    //     y: 87
+    // }, {
+    //     x: -1,
+    //     y: 9
+    // }, {
+    //     x: 0,
+    //     y: 1
+    // }, {
+    //     x: 1,
+    //     y: -3
+    // }, {
+    //     x: 2,
+    //     y: -21
+    // }];
+    drawUzd1NewtonTable();
 };
 
-function drawUzd1() {
+function drawUzd1NewtonTable() {
+    const drawAtDocument = document.getElementById('uzd1-newton-table');
+    let newtonTable = skaitiniaiMetodai.calcNewtonTable(data, data.length - 1);
+    newtonTable = newtonTable.map(e => e.map(e => e === '-' ? e : math.round(e)));
+    let table = {
+        type: 'table',
+        header: {
+            values: ['x', 'y'].concat(newtonTable.map((e, i) => 'f' + i))
+        },
+        cells: {
+            values: [
+                [...data.map(e => e.x)],
+                [...data.map(e => e.y)]
+            ].concat(newtonTable.map(e => e))
+        }
+    };
+    Plotly.react(drawAtDocument, [table]);
 }
