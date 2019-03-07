dbc2c69375bf46088234a088256466028c27c53a
Julius Lungys <juliuslungys@gmail.com>
2019-03-07 17:32:34 +0200

    feat(calculator): add Approx, Realerr and biases using Newton calculator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,52 +1,56 @@
+var skaitiniaiMetodai = new SkaitiniaiMetodai();
+skaitiniaiMetodai.roundingPrecision = 6;
 window.onload = () => {
-    autoFill();
-    document.getElementById('calculate-button').onclick = () => {
-        prepData();
-        drawTable();
+    autoFillNewt();
+    document.getElementById('newton-approximations-errors-biases-calculate-button').onclick = () => {
+        let data = prepDataNewt();
+        let newtonAproximations = skaitiniaiMetodai.calcApproximationsUsingNewton(data.X, data.xyArr);
+        let realErrors = skaitiniaiMetodai.calcRealErrors(newtonAproximations, data.Y);
+        let biases = skaitiniaiMetodai.calcBiases(newtonAproximations, data.Y);
+        let drawAtDocument = document.getElementById('newton-approximations-errors-biases');
+        drawTable(drawAtDocument,newtonAproximations,realErrors,biases);
     };
 };
 
-function autoFill() {
-    document.getElementById('x-table-values').value = "1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,2,2.1,2.2,2.3,2.4,2.5,2.6,2.7,2.8,2.9,3"
-    document.getElementById('y-table-values').value = "1.23 0.85 0.82 0.8 0.7 0.5 0.39 0.32 0.27 0.22 0.19 0.13 0.11 0.09 0.08 0.07 0.06 0.06 0.05 0.04";
-    //document.getElementById('X-value').value = 
-    //document.getElementById('Y-value').value =
+function autoFillNewt() {
+    document.getElementById('newton-approximations-errors-biases-x-table-values').value = '1 3 4 5 7 8 9 10';//'1,1.2,1.3,1.4,1.5,1.6,1.7,1.8,1.9,2,2.1,2.2,2.3,2.4,2.5,2.6,2.7,2.8,2.9,3';
+    document.getElementById('newton-approximations-errors-biases-y-table-values').value = '0.230 0.228 0.226 0.222 0.223 0.218 0.206 0.202'; //'1.23,0.85,0.82,0.8,0.7,0.5,0.39,0.32,0.27,0.22,0.19,0.13,0.11,0.09,0.08,0.07,0.06,0.06,0.05,0.04';
+    document.getElementById('newton-approximations-errors-biases-X-value').value = '2'; //'1.05';
+    document.getElementById('newton-approximations-errors-biases-Y-value').value = '0.231'; //'1';
 }
 
-function drawTable() {
-    document.getElementById('body')
-        .innerHTML += '<div id="table"><div>';
-}
-
-function prepData() {
-    let x = document.getElementById('x-table-values').value.split(',');
-    let y = document.getElementById('y-table-values').value.split(',');
-    let X = document.getElementById('X-value').value;
-    let Y = document.getElementById('Y-value').value;
-    return x.map((e, i) => {
+function prepDataNewt() {
+    let x = document.getElementById('newton-approximations-errors-biases-x-table-values').value.split(/[,\s]/);
+    let y = document.getElementById('newton-approximations-errors-biases-y-table-values').value.split(/[,\s]/);
+    let X = math.round(document.getElementById('newton-approximations-errors-biases-X-value').value, 15);
+    let Y = math.round(document.getElementById('newton-approximations-errors-biases-Y-value').value, 15);
+    let xyArr = x.map((e, i) => {
         return {
-            x: x[i],
-            y: y[i]
+            x: math.round(x[i], 15),
+            y: math.round(y[i], 15)
         };
     });
-    console.log(x,y,X,Y)
+    return {
+        X,
+        Y,
+        xyArr
+    };
+
 }
 
-function drawUzd1(data) {
-    let drawAtDocument = null;
+function drawTable(HTMLelement,approximations = [], realErrors = [], biases = []) {
+    HTMLelement.innerHTML += '<div id="result-table"><div>';
+    let drawAtDocument = document.getElementById('result-table');
     let tableValues = [];
-    let x = 2;
-    drawAtDocument = document.getElementById('uzd1-table-newt');
-    tableValues.push(calcApproximationValuesNewt(x, data));
-
+    tableValues.push(approximations);
     tableValues.unshift(tableValues[0].map((e, i) => i + 1));
-    tableValues.push(tableValues[1].map(e => math.abs(math.subtract(math.bignumber(0.231), e))));
-    tableValues.push(calcErrorValues(tableValues[1]));
+    tableValues.push(realErrors);
+    tableValues.push(biases);
 
     let table = {
         type: 'table',
         header: {
-            values: ['Eile', 'Artinys', 'Reali paklaida', 'Paklaidos ivertis']
+            values: ['Row', 'Approximation', 'Real error', 'Error bias']
         },
         cells: {
             values: tableValues
