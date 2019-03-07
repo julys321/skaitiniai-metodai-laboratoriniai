12818ec478bb70622050cdb7355ba4b1efaf946a
Julius Lungys <juliuslungys@gmail.com>
2019-03-07 14:19:30 +0200

    feat(calculator): add form

---
+++ calculator/calculator.js
@@ -0,0 +1,9 @@
+window.onload = () => {
+    document.getElementById('calculate-button').onclick = () => {
+        calculate();
+    };
+};
+function calculate() {
+    // document.getElementById('content')
+    //     .innerHTML = '<object type="text/html" data="calculator/calculator.html" ></object>';
+}
